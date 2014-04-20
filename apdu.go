b7c0ba7a15001a8f393409681e0cc13dc5aeab5c
Michael Gehring <mg@ebfe.org>
2014-04-20 18:38:00 +0200

    add le wildcards

--- apdu.go
+++ apdu.go
@@ -3,10 +3,23 @@ package main
 const (
 	apduMaxExtended = 0xffff + 1
 	apduMaxShort = 0xff + 1
+
+	leWildcard = -1
+	leWildcardExtended = -2
 )
 
 func encodeLength(n int, extended, first bool) []byte {
 	var encoded []byte
+
+	if n == leWildcard || n == leWildcardExtended {
+		if extended {
+			if first {
+				return []byte{0,0,0}
+			}
+			return []byte{0,0}
+		} 
+		return []byte{0}
+	}
 
 	if n < 0 || n > apduMaxExtended {
 		panic("apdu length out of range")
@@ -42,7 +55,7 @@ func encodeLength(n int, extended, first bool) []byte {
 
 func EncodeAPDU(cla, ins, p1, p2 byte, data []byte, le int) []byte {
 	var apdu []byte
-	var extended = len(data) > apduMaxShort || le > apduMaxShort
+	var extended = len(data) > apduMaxShort || le > apduMaxShort || le == leWildcardExtended
 
 	apdu = append(apdu, cla, ins, p1, p2)
 	apdu = append(apdu, encodeLength(len(data), extended, true)...)
